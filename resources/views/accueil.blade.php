59690328ebf3695ae8d86ee00773ac093c583909
HeleneMG <helenemouttetg@gmail.com>
2019-10-07 15:31:13 +0200

    landing page mobile

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -5,29 +5,41 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="<?php echo url('/assets/css/style.css') ?>">
+        <!-- FONTS -->
+        <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Montserrat|Roboto&display=swap" rel="stylesheet">
+        <!-- CSS MOBILE -->
+        <link rel="stylesheet" media="screen and (max-width: 425px)" href="<?php echo url('/assets/css/stylemobile.css') ?>">
+        <!-- CSS DESKTOP -->
+        <link rel="stylesheet" media="screen and (min-width: 426px)" href="<?php echo url('/assets/css/styledesktop.css') ?>">
         <title>Accueil - CityGuides</title>
     </head>
 
     <body>
-        <header>
-            <h1>WELCOME</h1>
-            <nav>
-                <ul>
-                    <li><a href="http://localhost/simplon/Laravel/cityGuides/public/">Accueil</a></li>
-                    <li><a href="http://localhost/simplon/Laravel/cityGuides/public/contact">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
         <main>
-            <section>
-                <h3>MA SECTION ACCUEIL</h3>
-            </section>
+            <div class="fond-noir">
+                <div class="close">
+                    <p>x</p>
+                </div>
+                <header>
+                    <h1>City Guides</h1>
+                    <h2>GUIDE AND VISIT !</h2>
+                </header>
+
+                <section class="inscription">
+                    <button>INSCRIPTION</button>
+                </section>
+
+                <section class="login">
+                    <h3><b>Déjà Inscrit.e ?</b></h3>
+                    <button>Connexion</button>
+                </section>
+            </div>
         </main>
+
         <footer>
             <p>tous droits réservés 2019</p>
         </footer>
-    </body>
-    
+        
     <script src="<?php echo url('/assets/js/main.js') ?>"></script>
+    </body>
 </html>
