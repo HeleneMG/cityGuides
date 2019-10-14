6eff034911747cc0be96a9e2c69462a9dbf6a888
HeleneMG <helenemouttetg@gmail.com>
2019-10-14 13:54:52 +0200

    smooth

--- resources/views/espace-admin.blade.php
+++ resources/views/espace-admin.blade.php
@@ -1,13 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Espace Admin - CityGuides</title>
-</head>
-<body>
-    <h1>espace admin</h1>
-    
-</body>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <script src="https://unpkg.com/scrollreveal"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="icon" type="image/png" href="assets/img/logo2.png" />
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Montserrat|Roboto&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo url('/assets/css/style-membre.css') ?>">
+        <title>Espace Membre - CityGuides</title>
+    </head>
+
+    <body>
+        <h1>espace admin</h1>
+
+    </body>
+
 </html>
--- resources/views/espace-membre.blade.php
+++ resources/views/espace-membre.blade.php
@@ -7,6 +7,7 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <script src="https://unpkg.com/scrollreveal"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <link rel="icon" type="image/png" href="assets/img/logo2.png" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Montserrat|Roboto&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo url('/assets/css/style-membre.css') ?>">
